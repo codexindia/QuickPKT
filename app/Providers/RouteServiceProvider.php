42f1da5c66c527edf2d5914454b17cd83c3f436b
codexindia <sudiptobain8@gmail.com>
2023-09-20 23:29:49 +0530

    bug

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,7 +41,7 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(10)->by(optional($request->user())->id ?: $request->ip())->response(function () {
                 return response([
-                    'messsage' => 'You have reached your access limit. Please try after 1 minute.',
+                    'message' => 'You have reached your access limit. Please try after 1 minute.',
                     'status' => false,
                     'limit_exceeded' => true, 
                     ], 429);
